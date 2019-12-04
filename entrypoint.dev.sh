c5be0ce74666433bc28a46378a92786672977343
Alexis Maiquez Murcia <almamu@almamu.com>
2019-12-04 11:16:28 +0100

    + added development dockerfiles
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

---
+++ entrypoint.dev.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+export PYTHONUNBUFFERED=TRUE
+
+# install cmake and build-essentials
+apt-get update && apt-get install -y cmake build-essential nginx
+
+# download places365 requirements
+cd /root/app/api/places365
+wget https://s3.eu-central-1.amazonaws.com/ownphotos-deploy/places365_model.tar.gz
+tar xf places365_model.tar.gz
+
+# download im2txt requirements
+cd /root/app/api/im2txt
+wget https://s3.eu-central-1.amazonaws.com/ownphotos-deploy/im2txt_model.tar.gz
+tar xf im2txt_model.tar.gz
+wget https://s3.eu-central-1.amazonaws.com/ownphotos-deploy/im2txt_data.tar.gz
+tar xf im2txt_data.tar.gz
+
+cd /root/app/
+
+# install prerequirements
+pip install -r prerequirements.txt
+# install requirements
+pip install -r requirements.txt
+
+python -m spacy download en_core_web_sm
+
+# cleanup installed packages
+apt-get remove --purge -y cmake git && rm -rf /var/lib/apt/lists/*
+
+# copy nginx configuration
+cp nginx.conf /etc/nginx/sites-enabled/default
+# run nginx as root - see https://github.com/hooram/ownphotos/issues/78
+sed -i -e 's/user www-data/user root/g' /etc/nginx/nginx.conf
+
+service nginx restart
+
+python image_similarity/main.py 2>&1 | tee logs/gunicorn_image_similarity.log &
+
+python manage.py makemigrations api 2>&1 | tee logs/command_makemigrations.log
+python manage.py migrate 2>&1 | tee logs/command_migrate.log
+python manage.py build_similarity_index 2>&1 | tee logs/command_build_similarity_index.log
+
+python manage.py shell <<EOF
+from api.models import User
+
+if User.objects.filter(username="$ADMIN_USERNAME").exists():
+    admin_user = User.objects.get(username="$ADMIN_USERNAME")
+    admin_user.set_password("$ADMIN_PASSWORD")
+    admin_user.save()
+else:
+    User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
+EOF
+
+echo "Running backend server..."
+
+python manage.py rqworker default 2>&1 | tee logs/rqworker.log &
+gunicorn --workers=2 --worker-class=gevent --bind 0.0.0.0:8000 --log-level=info ownphotos.wsgi 2>&1 | tee logs/gunicorn_django.log
+
